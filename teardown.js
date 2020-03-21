20173b5a44b3d44fb0632ab931e4c9d8ea10acb4
Daniel-Yonkov <daniel.yonkov@gmail.com>
2020-03-21 14:31:41 +0200

    Add teardown preset

---
+++ teardown.js
@@ -0,0 +1,39 @@
+const debug = require("debug")("jest-mysql:teardown");
+const { join } = require("path");
+
+const { query } = require("./helpers/mysql");
+const {
+    databaseOptions: { database }
+} = require(join(__dirname, "globalConfig.json"));
+module.exports = async function() {
+    debug("Teardown Mysql");
+
+    //removes foreing key checks in order to allow truncateing
+    debug("Staring database clearing");
+    await query("SET FOREIGN_KEY_CHECKS=0");
+
+    //truncates tables
+    const { results } = await query(`
+        SELECT Concat('TRUNCATE TABLE ',table_schema,'.',TABLE_NAME, ';') 
+        FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = "${database}"`);
+
+    //we truncate all the databases in the test environment
+    debug("Truncating tables");
+    results.map(async dbResults => {
+        for (const truncateQuery in dbResults) {
+            await query(dbResults[truncateQuery]);
+        }
+    });
+
+    //restore foreing key checks
+    await query("SET FOREIGN_KEY_CHECKS=1");
+    debug("Database Cleared");
+
+    debug("Stop initial mysql connection");
+    global.db.end(error => {
+        if (error) {
+            debug(error);
+        }
+        debug("Mysql connection terminated");
+    });
+};
