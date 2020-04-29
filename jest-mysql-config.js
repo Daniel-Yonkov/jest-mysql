28b75502b518d7643502c381f16fad2dd5010939
Daniel-Yonkov <daniel.yonkov@gmail.com>
2020-04-29 19:32:59 +0300

    Remove default config file from gitignore

---
+++ jest-mysql-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    databaseOptions: {
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "jest_mysql_test",
+        socketPath: "",
+        dateStrings: "DATETIME"
+    },
+    createDatabase: false,
+    dbSchema: "",
+    truncateDatabase: false,
+};
