08970cc1029b6977f86f12af9957ed9e3e530073
Daniel-Yonkov <daniel.yonkov@gmail.com>
2020-06-26 15:14:24 +0300

    Rework writeConfig to throw error if fail to copy

--- tests/fixtures/configWriter.js
+++ tests/fixtures/configWriter.js
@@ -2,7 +2,14 @@ const fs = require("fs");
 const { resolve } = require("path");
 
 async function writeConfig(file, filename = "jest-mysql-config.js") {
-    await fs.promises.copyFile(file, resolve(__dirname, `../../${filename}`));
+    try {
+        await fs.promises.copyFile(
+            file,
+            resolve(__dirname, `../../${filename}`)
+        );
+    } catch (e) {
+        throw new Error("Unable to copy config file");
+    }
 }
 
 async function writeObjectConfig(object = {}, filename = "globalConfig.json") {
